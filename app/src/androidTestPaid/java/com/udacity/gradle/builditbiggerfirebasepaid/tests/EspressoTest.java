ac872520b59d544808edc7260459d1921f821fd8
Annin7y <anast3222@gmail.com>
2019-12-24 00:59:24 -0500

    update code: Espresso Test paid package class

--- app/src/androidTestPaid/java/com/udacity/gradle/builditbiggerfirebasepaid/tests/EspressoTest.java
+++ app/src/androidTestPaid/java/com/udacity/gradle/builditbiggerfirebasepaid/tests/EspressoTest.java
@@ -1,4 +1,37 @@
 package java.com.udacity.gradle.builditbiggerfirebasepaid.tests;
 
-public class EspressoTest {
+import androidx.test.runner.AndroidJUnit4;
+
+import com.udacity.gradle.builditbiggerfirebase.MainActivity;
+import com.udacity.gradle.builditbiggerfirebase.R;
+
+@RunWith(AndroidJUnit4.class)
+public class EspressoTest
+{
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void checkTextView_isDisplayed_and_notEmpty() throws Exception {
+        // perform a click on the button
+        onView(withId(R.id.joke_button)).perform(click());
+
+        // passes if the textView does not match the empty string
+        onView(withId(R.id.joke_android)).check(matches(not(withText(""))));
+    }
+
+    //https://stackoverflow.com/questions/32906881/checking-if-a-button-is-clickable-in-espresso-test-android-studio
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void buttonIsEnabled()
+    {
+        onView(withId(R.id.joke_button)).check(matches(isClickable()));
+    }
+
 }
