9fec9b304344103c86113f2d6fda1a0568bc9334
Annin7y <anast3222@gmail.com>
2019-12-25 02:38:00 -0500

    update code: Espresso Test paid package class

--- app/src/androidTestPaid/java/com/udacity/gradle/builditbiggerfirebasepaid/tests/EspressoTest.java
+++ app/src/androidTestPaid/java/com/udacity/gradle/builditbiggerfirebasepaid/tests/EspressoTest.java
@@ -1,9 +1,19 @@
-package java.com.udacity.gradle.builditbiggerfirebasepaid.tests;
+package com.udacity.gradle.builditbiggerfirebasepaid.tests;
 
+import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import com.udacity.gradle.builditbiggerfirebase.MainActivity;
 import com.udacity.gradle.builditbiggerfirebase.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class EspressoTest
@@ -13,19 +23,6 @@ public class EspressoTest
     public ActivityTestRule<MainActivity> mActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
-    @Test
-    public void checkTextView_isDisplayed_and_notEmpty() throws Exception {
-        // perform a click on the button
-        onView(withId(R.id.joke_button)).perform(click());
-
-        // passes if the textView does not match the empty string
-        onView(withId(R.id.joke_android)).check(matches(not(withText(""))));
-    }
-
-    //https://stackoverflow.com/questions/32906881/checking-if-a-button-is-clickable-in-espresso-test-android-studio
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule =
-            new ActivityTestRule<>(MainActivity.class);
 
 
     @Test
